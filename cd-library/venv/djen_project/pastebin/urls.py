4adf04defbe36d666462a69efcb80d1f0937a82f
TheoOliveira <theophilo.oliveira@gmail.com>
2020-05-20 01:47:43 -0300

    modified:   venv/djen_project/pastebin/models.py 	new file:   venv/djen_project/pastebin/templates/pastebin/paste_detail.html 	modified:   venv/djen_project/pastebin/urls.py 	modified:   venv/djen_project/pastebin/views.py

--- cd-library/venv/djen_project/pastebin/urls.py
+++ cd-library/venv/djen_project/pastebin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PasteCreate
+from .views import PasteCreate, PasteDetail
+
 
 urlpatterns = [
     path(r'', PasteCreate.as_view(), name='create'),
+    path('paste/<int:pk>', PasteDetail.as_view(), name='detail')
 ]
--- cd-library/venv/djen_project/pastebin/views.py
+++ cd-library/venv/djen_project/pastebin/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView,DetailView
 from .models import Paste
 # Create your views here.
 
 class PasteCreate(CreateView):
-    model = Paste 
+    model = Paste
     fields = ['texto', 'nome']
 
+class PasteDetail(DetailView):
+    model = Paste
+    template_name = 'pastebin/paste_detail.html'
